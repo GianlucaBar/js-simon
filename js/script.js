603cb2625510babb2f2105605317cfe25abfed69
GianlucaBar <gianlucabarce@gmail.com>
2021-04-07 15:38:01 +0200

    completato (per davvero)

--- js/script.js
+++ js/script.js
@@ -1,42 +1,53 @@
 // popolo un array con 5 numeri random e li stampo in alert
-var numberList = []
+
+var difficulty = 5
+var numberList = randomNumbersGen(1, 100, difficulty);
+
+alert(numberList);
 
 
-while ( numberList.length < 5 ){
-    var rndNumber = getRndInteger(1, 100);
-
-    if (numberList.includes(rndNumber) == false){
-        numberList.push(rndNumber);
-    }
-}
 
 // console.log(numberList)
+
+
 //========
 //dopo 30 secondi
+//========
+
 setTimeout(function() {
     // chiedo all'utente di inserire un numero in prompt 5 volte
     var correctNumbers = [];
-    for (var i = 0; i < 5; i++){
+    for (var i = 0; i < difficulty; i++){
         var thisNumeroUtente = parseInt(prompt("inserisci un numero che ricordi"));
+
         // ogni numero utente va confrontato con l'array di numeri random
         //se e' presente, salvo il numero in un array "numeritrovati"
-        if (numberList.includes(thisNumeroUtente)){
+        if (numberList.includes(thisNumeroUtente) && correctNumbers.includes(thisNumeroUtente) == 0 ){
             correctNumbers.push(thisNumeroUtente);
         }
     }
     
     // stampo l'array e la sua lunghezza
     alert("hai ricordato " + correctNumbers.length + " numeri: " + correctNumbers)
-}, 30000);
-
-// ogni numero utente va confrontato con l'array di numeri random
-//se e' presente, salvo il numero in un array "numeritrovati"
-
-// stampo l'array e la sua lunghezza 
+}, 3000);
 
 
-
-
+// ============
+// FUNCTIONS 
+// ============
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
   }
+
+function randomNumbersGen(min, max, amount){
+    var rndList = []
+    while ( rndList.length < amount ){
+        var rndNumber = getRndInteger(min, max);
+    
+        if (rndList.includes(rndNumber) == false){
+            rndList.push(rndNumber);
+        }
+    }
+
+    return rndList;
+}
